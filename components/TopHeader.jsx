20d4c80c44d058987709a51a140169fbaaaa875e
Bigyan Darshan Patel <bigyanpatel07769@gmail.com>
2023-12-26 12:15:04 +0530

    Init: header section

---
+++ components/TopHeader.jsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import React from "react";
+import { FaPhoneAlt } from "react-icons/fa";
+import { IoIosMail } from "react-icons/io";
+
+const TopHeader = () => {
+  return (
+    <div className="w-full bg-[#FAF1E6] flex items-center justify-between px-4 py-2 sm:px-24">
+      <div className="flex items-center gap-4">
+        <div className="flex gap-2 items-center text-sm">
+          <FaPhoneAlt className="text-[#27957E]" />
+          <p>[+91] 7847587934</p>
+        </div>
+        <div className="flex gap-2 items-center text-sm">
+          <IoIosMail className="text-[#27957E]" />
+          <p>schooling@gmail.com</p>
+        </div>
+      </div>
+      <div className="flex items-center gap-4 text-sm">
+        <Link href="#">Alumini</Link>
+        <Link href="#">Calender</Link>
+        <Link href="#">Email Login</Link>
+      </div>
+    </div>
+  );
+};
+
+export default TopHeader;
