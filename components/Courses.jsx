3879d2f2c2d7668590a74c95d2eb01adc1c7d396
Bigyan Darshan Patel <bigyanpatel07769@gmail.com>
2023-12-27 12:36:19 +0530

    Style: courses section responsiveness

--- components/Courses.jsx
+++ components/Courses.jsx
@@ -4,19 +4,19 @@ import Image from "next/image";
 
 const Courses = () => {
   return (
-    <div className="bg-gray-800 w-full p-16">
-      <div className="w-full flex">
-        <h1 className="w-1/2 font-bold text-5xl text-white pr-6">
+    <div className="bg-gray-800 w-full p-8 sm:p-[10] lg:p-16">
+      <div className="w-full flex flex-col sm:flex-row gap-4">
+        <h1 className="w-full sm:w-1/2 font-bold text-3xl sm:text-5xl text-white pr-6">
           Upgrade Your Skill, Upgrade Your Life
         </h1>
-        <p className="w-1/2 text-gray-400 px-[40px] flex items-center">
+        <p className="w-full sm:w-1/2 text-gray-400 sm:px-[40px] flex items-center">
           The following are some subjects that students will study when they
           enter our school. Of course our subjects are based on the latest
           curriculum.
         </p>
       </div>
-      <div className="w-full mt-14 flex">
-        <div className="w-1/2 flex flex-col gap-4">
+      <div className="w-full mt-14 flex flex-col lg:flex-row gap-4">
+        <div className="w-full lg:w-1/2 flex flex-col gap-4 order-2 lg:order-1">
           <div className="flex flex-col gap-4">
             <div className="flex gap-4">
               <p className="text-blue-500 font-semibold text-lg flex items-end">
@@ -74,7 +74,7 @@ const Courses = () => {
             </p>
           </div>
         </div>
-        <div className="w-1/2 p-10">
+        <div className="w-full lg:w-1/2 lg:p-10 order-1 lg:order-2 h-80 lg:h-[500px]">
           <div className="w-full h-full relative rounded-xl overflow-hidden">
             <Image
               src={syllabus}
