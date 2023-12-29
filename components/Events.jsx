b5d6799dc2cf4a4084cf1949e546d71440250ea6
Bigyan Darshan Patel <bigyanpatel07769@gmail.com>
2023-12-29 13:21:26 +0530

    Feat: Event section with custom modal view

--- components/Events.jsx
+++ components/Events.jsx
@@ -1,81 +1,86 @@
 import Image from "next/image";
-import Link from "next/link";
-import React from "react";
-import { FaArrowRight } from "react-icons/fa";
-import syllabus from "@/assests/images/syllabus.jpg";
+import React, { useState } from "react";
+import Dialog from "./Modal";
+import { events } from "@/constants";
 
 const Events = () => {
+  const [open, setOpen] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
+  const onClose = () => {
+    setOpen(false);
+    setSelectedEvent(null);
+  };
+
+  const handleEventDetails = (event) => {
+    setSelectedEvent(event);
+    setOpen(true);
+  };
+
   return (
     <div className="w-full p-4 sm:p-10 lg:p-[80px]">
       <div className="w-full flex justify-between items-center">
         <h1 className="font-bold text-3xl">School News and Updates</h1>
-        <Link href="#" className="text-green-500 flex items-center gap-4 min-w-[120px] text-sm sm:text-base">
+        {/* <Link
+          onClick={() => setOpen(true)}
+          className="text-green-500 flex items-center gap-4 min-w-[120px] text-sm sm:text-base"
+        >
           View All News
           <FaArrowRight />
-        </Link>
+        </Link> */}
       </div>
       <div className="flex gap-8 mt-7 flex-nowrap overflow-x-auto">
-        <div className="min-w-[300px] max-w-[300px] flex flex-col gap-4">
-          <Image src={syllabus} alt="test photo" width={300} height={300} />
-          <h3 className="font-semibold text-xl">Lorem ipsum dolor sit amet.</h3>
-          <h6 className="text-sm">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt
-            neque hic nostrum? Vel aliquam aspernatur temporibus tenetur
-            consequuntur, labore perspiciatis!
-          </h6>
-          <Link href="#" className="px-4 py-2 bg-green-700 text-white w-fit">
-            Read More
-          </Link>
-        </div>
-        <div className="min-w-[300px] max-w-[300px] flex flex-col gap-4">
-          <Image src={syllabus} alt="test photo" width={300} height={300} />
-          <h3 className="font-semibold text-xl">Lorem ipsum dolor sit amet.</h3>
-          <h6 className="text-sm">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt
-            neque hic nostrum? Vel aliquam aspernatur temporibus tenetur
-            consequuntur, labore perspiciatis!
-          </h6>
-          <Link href="#" className="px-4 py-2 bg-green-700 text-white w-fit">
-            Read More
-          </Link>
-        </div>
-        <div className="min-w-[300px] max-w-[300px] flex flex-col gap-4">
-          <Image src={syllabus} alt="test photo" width={300} height={300} />
-          <h3 className="font-semibold text-xl">Lorem ipsum dolor sit amet.</h3>
-          <h6 className="text-sm">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt
-            neque hic nostrum? Vel aliquam aspernatur temporibus tenetur
-            consequuntur, labore perspiciatis!
-          </h6>
-          <Link href="#" className="px-4 py-2 bg-green-700 text-white w-fit">
-            Read More
-          </Link>
-        </div>
-        <div className="min-w-[300px] max-w-[300px] flex flex-col gap-4">
-          <Image src={syllabus} alt="test photo" width={300} height={300} />
-          <h3 className="font-semibold text-xl">Lorem ipsum dolor sit amet.</h3>
-          <h6 className="text-sm">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt
-            neque hic nostrum? Vel aliquam aspernatur temporibus tenetur
-            consequuntur, labore perspiciatis!
-          </h6>
-          <Link href="#" className="px-4 py-2 bg-green-700 text-white w-fit">
-            Read More
-          </Link>
-        </div>
-        <div className="min-w-[300px] max-w-[300px] flex flex-col gap-4">
-          <Image src={syllabus} alt="test photo" width={300} height={300} />
-          <h3 className="font-semibold text-xl">Lorem ipsum dolor sit amet.</h3>
-          <h6 className="text-sm">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt
-            neque hic nostrum? Vel aliquam aspernatur temporibus tenetur
-            consequuntur, labore perspiciatis!
-          </h6>
-          <Link href="#" className="px-4 py-2 bg-green-700 text-white w-fit">
-            Read More
-          </Link>
-        </div>
+        {events.map((eventContent) => {
+          return (
+            <div
+              key={eventContent.id}
+              className="min-w-[300px] max-w-[300px] flex flex-col gap-4"
+            >
+              <Image
+                src={eventContent.image}
+                alt="test photo"
+                width={300}
+                height={300}
+              />
+              <h3 className="font-semibold text-xl">{eventContent.title}</h3>
+              <h6 className="text-sm line-clamp-3">
+                {eventContent.description}
+              </h6>
+              <button
+                onClick={() => handleEventDetails(eventContent)}
+                className="px-4 py-2 bg-green-700 text-white w-fit"
+              >
+                Read More
+              </button>
+            </div>
+          );
+        })}
       </div>
+      <Dialog
+        className="lg:p-0 p-2"
+        contentClassName="w-full h-screen bg-white p-3 lg:p-6 max-w-3xl rounded-lg"
+        overlayClassName="backdrop-blur"
+        isOpen={open}
+        closable={true}
+        onClose={onClose}
+        onRequestClose={onClose}
+      >
+        {selectedEvent && (
+          <div className="flex flex-col gap-6">
+            <h2 className="text-2xl font-semibold">{selectedEvent.title}</h2>
+            <div className="relative h-80 lg:h-96 rounded-lg overflow-hidden">
+              <Image
+                src={selectedEvent.image}
+                alt={selectedEvent.title}
+                layout="fill"
+                objectFit="cover"
+                className="rounded-lg"
+              />
+            </div>
+            <p className="text-gray-600">{selectedEvent.description}</p>
+          </div>
+        )}
+      </Dialog>
     </div>
   );
 };
---
+++ constants.js
@@ -0,0 +1,85 @@
+import eventSample1 from "@/assests/images/eventSample1.jpg"
+import eventSample2 from "@/assests/images/eventSample2.jpg"
+import eventSample3 from "@/assests/images/eventSample3.jpg"
+import eventSample4 from "@/assests/images/eventSample4.jpg"
+import eventSample5 from "@/assests/images/eventSample5.jpg"
+import eventSample6 from "@/assests/images/eventSample6.jpg"
+import eventSample7 from "@/assests/images/eventSample7.jpg"
+import eventSample8 from "@/assests/images/eventSample8.jpg"
+import eventSample9 from "@/assests/images/eventSample9.jpg"
+import eventSample10 from "@/assests/images/eventSample10.jpg"
+
+const events = [
+    {
+        id: 1,
+        title: "Tech Expo 2023",
+        description:
+            "Explore the future at our Tech Expo! Witness the latest innovations, gadgets, and technological breakthroughs from leading tech companies and startups. Engage in tech demos, hands-on workshops, and panel discussions featuring tech experts. Experience the fusion of cutting-edge technologies like AI, IoT, and robotics. Discover how technology is shaping industries, revolutionizing businesses, and influencing everyday lives. From smart homes to futuristic transportation, immerse yourself in the world of possibilities. Our Tech Expo is a hub for networking, collaboration, and exploring disruptive technologies that drive change.",
+        image: eventSample1,
+    },
+    {
+        id: 2,
+        title: "Art & Music Festival",
+        description:
+            "Join us for a weekend filled with artistic expressions! Enjoy live music performances, art exhibitions, workshops, and interactive sessions. Immerse yourself in diverse art forms and musical genres. Our festival celebrates creativity, uniting artists and art enthusiasts from different backgrounds. Explore various art styles, from classical to contemporary, and witness the creative process firsthand. Engage in interactive art installations and participate in community art projects. The festival serves as a platform to discover emerging talents and appreciate established artists. Connect with like-minded individuals passionate about art and music, fostering a vibrant cultural experience.",
+        image: eventSample2,
+    },
+    {
+        id: 3,
+        title: "Science Symposium",
+        description:
+            "The Science Symposium features distinguished scientists and researchers sharing insights on cutting-edge discoveries and scientific advancements. Dive deep into various scientific fields, from quantum physics to environmental science. Explore the intricacies of scientific research methodologies and their real-world applications. Engage in discussions about the latest breakthroughs in medicine, space exploration, and sustainable technologies. The symposium fosters intellectual discussions and collaborations, inviting curious minds to explore the frontiers of science.",
+        image: eventSample3,
+    },
+    {
+        id: 4,
+        title: "Cultural Diversity Conference",
+        description:
+            "Celebrate diversity! Engage in discussions, cultural showcases, and presentations highlighting the importance of embracing cultural differences. Explore the rich tapestry of cultures and traditions. Delve into the essence of cultural diversity, promoting mutual understanding and respect. The conference aims to bridge cultural gaps, fostering inclusivity and global citizenship. Experience diverse cuisines, traditional performances, and cultural exhibitions representing various ethnicities and heritage.",
+        image: eventSample4,
+    },
+    {
+        id: 5,
+        title: "Business Summit",
+        description:
+            "Connect with industry leaders and entrepreneurs at our Business Summit. Gain valuable insights, network, and explore new business opportunities. Attend workshops, pitch competitions, and keynote speeches. Learn about the latest market trends, strategies for business growth, and disruptive innovations. Engage in discussions on global markets, digital transformation, and sustainable business practices. The summit provides a platform for collaboration, empowering aspiring entrepreneurs and established businesses alike.",
+        image: eventSample5,
+    },
+    {
+        id: 6,
+        title: "Health & Wellness Expo",
+        description:
+            "Prioritize your health! Visit our Health & Wellness Expo featuring fitness activities, wellness workshops, and expert health advice. Learn about holistic wellness and healthy lifestyle choices. Participate in yoga sessions, meditation workshops, and nutritional seminars. Explore the latest advancements in healthcare technology and alternative therapies. The expo focuses on physical, mental, and emotional well-being, encouraging individuals to adopt healthier lifestyles.",
+        image: eventSample6,
+    },
+    {
+        id: 7,
+        title: "Environmental Conference",
+        description:
+            "Join the movement for a sustainable future! The Environmental Conference focuses on environmental challenges and solutions. Participate in discussions on conservation, renewable energy, and eco-friendly initiatives. Learn from environmental experts about biodiversity conservation, climate change mitigation, and sustainable development goals. Explore innovative approaches to environmental stewardship and advocate for a greener, more sustainable planet.",
+        image: eventSample7,
+    },
+    {
+        id: 8,
+        title: "Education Summit",
+        description:
+            "Shape the future of education! Engage in discussions about educational reforms, innovations, and strategies to enhance learning. Discover new teaching methodologies and technologies. Explore the role of educators in shaping the next generation. The summit aims to address challenges in education, focusing on inclusive learning, digital literacy, and personalized learning experiences. Join educators, policymakers, and thought leaders in envisioning the future of education.",
+        image: eventSample8,
+    },
+    {
+        id: 9,
+        title: "Fashion Show Extravaganza",
+        description:
+            "Experience style and glamour! Our Fashion Show brings together top designers and models showcasing the latest fashion trends. Witness runway spectacles and behind-the-scenes fashion insights. Explore the fusion of cultural influences and avant-garde fashion. Engage in discussions on sustainable fashion practices and the future of the fashion industry. The extravaganza celebrates creativity and individual expression through the artistry of fashion.",
+        image: eventSample9,
+    },
+    {
+        id: 10,
+        title: "Food Festival",
+        description:
+            "Indulge in a gastronomic delight! Enjoy a variety of cuisines, food tastings, cooking demonstrations, and culinary competitions. Discover culinary secrets from renowned chefs. Immerse yourself in a food lover's paradise with diverse flavors and gastronomic experiences. Explore global culinary traditions, local delicacies, and innovative culinary creations. The festival celebrates the art of cooking and the joy of sharing meals.",
+        image: eventSample10,
+    },
+];
+
+export { events };
