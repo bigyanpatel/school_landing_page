4dcb4426926721096a708135a94e28f0a0a060ba
Bigyan Darshan Patel <bigyanpatel07769@gmail.com>
2024-01-02 11:18:40 +0530

    Refactor: Gallery page content update dynamically

--- pages/gallery.jsx
+++ pages/gallery.jsx
@@ -3,10 +3,37 @@ import Image from "next/image";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import TopHeader from "@/components/TopHeader";
-import eventSample1 from "@/assests/images/eventSample1.jpg";
 import eventSample3 from "@/assests/images/eventSample3.jpg";
+import { createClient } from "contentful";
 
-const GalleryPage = () => {
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_KEY,
+  });
+
+  try {
+    const response = await client.getEntries({
+      content_type: "gallery",
+    });
+
+    return {
+      props: {
+        galleryItems: response.items || [],
+      },
+      revalidate: 60, // Adjust revalidation period as needed
+    };
+  } catch (error) {
+    console.error("Error fetching gallery items:", error);
+    return {
+      props: {
+        galleryItems: [],
+      },
+    };
+  }
+}
+
+const GalleryPage = ({ galleryItems }) => {
   return (
     <>
       <TopHeader />
@@ -19,24 +46,24 @@ const GalleryPage = () => {
           className="w-full h-full object-cover object-center absolute"
         />
         <h1 className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-white text-4xl sm:text-6xl font-bold">
-            Gallery
+          Gallery
         </h1>
       </div>
       <div className="bg-gray-200 py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-5xl mx-auto flex flex-wrap justify-evenly gap-6">
-          {Array.from({ length: 5 }).map((img, index) => (
+          {galleryItems.map((item, index) => (
             <div
               key={index}
               className="w-full h-[300px] max-w-[400px] rounded-lg overflow-hidden shadow-md group relative"
             >
               <Image
-                src={eventSample1}
+                src={"https:" + item.fields.photo.fields.file.url}
                 alt={`Gallery Image ${index + 1}`}
                 fill
                 className="object-cover object-center absolute"
               />
               <h2 className="group-hover:opacity-100 opacity-0 text-white text-xl font-semibold absolute duration-500 bottom-4 left-4">
-                Test Image Title
+                {item.fields.title}
               </h2>
             </div>
           ))}
