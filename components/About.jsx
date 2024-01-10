e38e05f9212a4425e456a261949f3a3a12ba271f
Bigyan Darshan Patel <bigyanpatel07769@gmail.com>
2024-01-10 09:46:18 +0530

    Refactor: Teacher and principal images about

--- components/About.jsx
+++ components/About.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import principal from "@/assests/images/college_principal.jpeg";
+import teacher_1 from "@/assests/images/college_teacher.jpeg";
 import Image from "next/image";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
 const About = () => {
   return (
@@ -9,19 +11,7 @@ const About = () => {
       id="about"
       className="flex flex-col sm:flex-row w-full p-10 md:p-14 lg:p-[100px] gap-10 sm:gap-6"
     >
-        <div className="w-full sm:w-1/2 h-fit lg:pr-[100px] order-2 sm:order-1 relative">
-          <div className="w-full h-[300px] sm:h-[500px] relative">
-            <Image
-              src={principal}
-              alt="Long school corridor"
-              fill
-              className="w-full h-full absolute object-cover object-center"
-            />
-          </div>
-          <h5 className="px-2 text-xl font-bold mt-2">Dr. Pradeepta Ku. Dash</h5>
-          <h5 className="px-2 text-lg font-base">Principal</h5>
-        </div>
-      {/* <Carousel
+      <Carousel
         swipeable={true}
         showIndicators={true}
         showArrows={true}
@@ -32,8 +22,35 @@ const About = () => {
         infiniteLoop
         autoPlay
       >
-
-      </Carousel> */}
+        <div className="w-full h-[300px] sm:h-[500px] relative">
+          <Image
+            src={principal}
+            alt="Principal"
+            fill
+            className="w-full h-full absolute object-cover object-center"
+          />
+          <div className="absolute bottom-6 left-1/2 -translate-x-1/2 text-white w-full">
+            <h5 className="px-2 text-base sm:text-xl font-bold mt-2">
+              Dr. Pradeepta Ku. Dash
+            </h5>
+            <h5 className="px-2 text-sm sm:text-lg font-base">Principal</h5>
+          </div>
+        </div>
+        <div className="w-full h-[300px] sm:h-[500px] relative">
+          <Image
+            src={teacher_1}
+            alt="Long school corridor"
+            fill
+            className="w-full h-full absolute object-cover object-center"
+          />
+          <div className="absolute bottom-6 left-1/2 -translate-x-1/2 text-white">
+            <h5 className="px-2 text-base sm:text-xl font-bold mt-2">
+              Dr. Ramesh Chandra Behera
+            </h5>
+            <h5 className="px-2 text-sm sm:text-lg font-base">President, GB</h5>
+          </div>
+        </div>
+      </Carousel>
       <div className="relative w-full sm:w-1/2 flex flex-col justify-between gap-4 order-1 sm:order-2">
         <h1 className="font-semibold text-3xl">About our College</h1>
         <article>
