f80331eec839e6411b1dc9bedbbe102c07281a78
Bigyan Darshan Patel <bigyanpatel07769@gmail.com>
2024-01-10 09:47:14 +0530

    Style: top Header sizing

--- components/TopHeader.jsx
+++ components/TopHeader.jsx
@@ -7,20 +7,15 @@ const TopHeader = () => {
   return (
     <div className="w-full bg-[#FAF1E6] flex flex-col sm:flex-row items-center justify-between px-4 py-2 sm:px-24">
       <div className="flex items-center gap-4">
-        <div className="flex gap-2 items-center text-sm">
+        <div className="flex gap-2 items-center text-xs sm:text-sm">
           <FaPhoneAlt className="text-[#27957E]" />
           <p>[+91] 8328921955</p>
         </div>
-        <div className="flex gap-2 items-center text-sm">
+        <div className="flex gap-2 items-center text-xs sm:text-sm">
           <IoIosMail className="text-[#27957E]" />
           <p>principal_bpdc@yahoo.com</p>
         </div>
       </div>
-      {/* <div className="flex items-center gap-4 text-sm">
-        <Link href="#">Alumini</Link>
-        <Link href="#">Calender</Link>
-        <Link href="#">Email Login</Link>
-      </div> */}
     </div>
   );
 };
